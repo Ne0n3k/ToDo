056a8c458b5dc4670ce6f23600991e5397450371
Neonek <jakub.blazowski@gmail.com>
2025-08-21 15:17:38 +0200

    SwiftUI App structure and base color assets prepared

---
+++ ToDo/ToDo/ToDoTests/ToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTests.swift
+//  ToDoTests
+//
+//  Created by Jakub Błażowski on 21/08/2025.
+//
+
+import Testing
+@testable import ToDo
+
+struct ToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
